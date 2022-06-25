3cdfa77c9a8d46a62ac5abd6b22c3581b690b170
Taras Prystuplyk <1988taras@ukr.net>
2022-06-26 02:37:41 +0300

    Start

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import Profile from "./profile/profile";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <div>
+      <Profile
+        //  username={user.username}
+        //  tag={user.tag}
+        //  location={user.location}
+        //  avatar={user.avatar}
+        //  stats={user.stats}
+         />
+
+      </div>
+      
+      {/* React homework template */}
+    
+    </div>
+  );
+};
