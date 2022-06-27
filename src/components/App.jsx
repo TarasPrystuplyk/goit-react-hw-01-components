303619a9a7362ad0748790ca466608af0d2fa77a
Taras Prystuplyk <1988taras@ukr.net>
2022-06-27 23:15:05 +0300

    All components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
-import user from "./json/user.json";
-import Profile from "./profile/profile";
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+import FriendList from './friends/FriendList';
+import TransactionHistory from './transactions/Transactions';
 
 export const App = () => {
   return (
@@ -22,6 +29,18 @@ export const App = () => {
          stats={user.stats}
          />
 
+      </div>
+
+      <div>
+        <Statistics title="Upload stats" stats={data} />
+      </div>
+
+      <div>
+        <FriendList friends={friends} />
+      </div>
+
+      <div>
+        <TransactionHistory items={transactions} />
       </div>
       
       {/* React homework template */}
---
+++ src/components/friends/FriendList.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul class="friend-list">
+  <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+
+</ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.array.isRequired,
+};
