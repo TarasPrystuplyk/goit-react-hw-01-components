e5a1f5793ec764c8f19d0d18caf9b86ebcc99285
Taras Prystuplyk <1988taras@ukr.net>
2022-06-28 22:26:24 +0300

    profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -41,6 +41,7 @@ export default function Profile ({username, tag, location, avatar, stats }) {
   );
 }
 
+
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
